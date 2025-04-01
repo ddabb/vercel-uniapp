b7725752b044912ec60a8cf0713ea693a81b37dc
60138546 <1011888891@qq.com>
2025-04-01 14:39:08 +0800

    修复数据

--- vite.config.js
+++ vite.config.js
@@ -7,57 +7,47 @@ import path from 'path';
 const base = process.env.UNI_PLATFORM === 'h5' ? '/' : './';
 
 export default defineConfig({
-  base: base,
-  plugins: [
-    nodePolyfills({
-      include: ['path', 'fs', 'util', 'stream', 'constants', 'assert'],
-      globals: { Buffer: true, process: true }
-    }),
-    uni()
-  ],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src')
-    }
-  },
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://www.60score.com',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '/api')
-      },
-      '^/[^/]+$': {
-        target: 'http://localhost:5173',
-        rewrite: (path) => `/public${path}`,
-        changeOrigin: true
-      }
-    },
-    static: {
-      dirs: ['public']
-    },
-    hmr: {
-      overlay: true  // 显示完整的错误覆盖层
-    }
-  },
-  build: {
-    sourcemap: true,
-    target: 'esnext',
-    // 动态调整 lib 配置
-    ...(process.env.UNI_PLATFORM !== 'h5' && {
-      lib: {
-        name: 'main',
-        entry: 'src/main.js',
-        formats: ['es']
-      }
-    }),
-    rollupOptions: {
-      external: ['vue', 'qrcode'],
-      output: {
-        globals: {
-          vue: 'Vue'
+    base: base,
+    plugins: [
+        nodePolyfills({
+            include: ['path', 'fs', 'util', 'stream', 'constants', 'assert'],
+            globals: { Buffer: true, process: true }
+        }),
+        uni()
+    ],
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src')
         }
-      }
+    },
+    server: {
+        proxy: {
+            '/api': {
+                target: 'https://www.60score.com',
+                changeOrigin: true,
+                rewrite: (path) => path.replace(/^\/api/, '/api')
+            },
+            '^/[^/]+$': {
+                target: 'http://localhost:5173',
+                rewrite: (path) => `/public${path}`,
+                changeOrigin: true
+            }
+        },
+        static: {
+            dirs: ['public']
+        }
+    },
+    build: {
+        target: 'esnext', // 修改为更兼容的版本
+        assetsInlineLimit: 4096,
+        // 动态调整 lib 配置
+        ...(process.env.UNI_PLATFORM !== 'h5' && {
+            lib: {
+                name: 'main',
+                entry: 'src/main.js',
+                formats: ['es']
+            }
+        })
     }
-  }
-})
+}
+);
