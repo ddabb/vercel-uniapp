e886d8cd94afb13ed9dec7fd493e0b081eb1e202
ddabb <417144452@qq.com>
2025-04-03 21:51:24 +0800

    还原到vite版本

---
+++ vite.config.js
@@ -0,0 +1,44 @@
+import { defineConfig } from 'vite';
+import uni from '@dcloudio/vite-plugin-uni';
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
+import path from 'path';
+
+export default defineConfig({
+  plugins: [
+    nodePolyfills({
+      include: ['path', 'fs', 'util', 'stream', 'constants', 'assert'],
+      globals: { Buffer: true, process: true }
+    }),
+    uni()
+  ],
+  resolve: {
+    alias: {
+        '@': path.resolve(__dirname, './src').replace(/\\/g, '/') // 转换为正斜杠[7](@ref)
+    }
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://www.60score.com',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '/api')
+      },
+      '^/[^/]+$': {
+        target: 'http://localhost:5173',
+        rewrite: (path) => `/public${path}`,
+        changeOrigin: true
+      }
+    },
+    static: {
+      dirs: ['public']
+    }
+  },
+  build: {
+    target: 'esnext', // 修改为更兼容的版本
+    assetsInlineLimit: 4096
+
+    // lib: { name: 'main', entry: 'src/main.js', formats: ['es']
+    }
+
+  }
+);    
